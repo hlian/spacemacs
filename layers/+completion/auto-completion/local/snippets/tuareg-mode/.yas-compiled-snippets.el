da65bc948b5c8df3afdc3b61be6b82f669a03a64
Hao Lian <hi@haolian.org>
2021-01-04 11:29:44 -0500

    snippets

---
+++ layers/+completion/auto-completion/local/snippets/tuareg-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `tuareg-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'tuareg-mode
+                     '(("ww" "(* CR hlian: fix me *)\n[@@@ocaml.warning \"-32\"]\n" "warnings" nil nil nil "/home/hlian/.emacs.d/layers/+completion/auto-completion/local/snippets/tuareg-mode/ww" nil nil)))
+
+
+;;; Do not edit! File generated at Mon Oct 19 11:38:20 2020
